706e8814ab8b9774ffc13cb4e948fc37aa8e9bb3
Santosh Narwade <santosh.narwade@canstar.com.au>
2023-01-19 18:48:24 +0530

    contactus page ready

--- submit.php
+++ submit.php
@@ -1,16 +1,19 @@
 <?php
 
 $name = $_POST['name'];
-$email = $_POST['email'];
-$body = $_POST['subject'];
+$phone = $_POST['phone'];
+$email = $_POST['address'];
+$subject = $_POST['subject'];
+$body = $_POST['Message'];
 
 $to = "santosh.narawade1@gmail.com";
-$subject = "LaxZenith :: Contact Us for Filled";
-$txt = "Hello world!";
+$subject = "LaxZenith :: Contact Us for Filled ". $subject;
+
 $headers = "From: ".$email . "\r\n" ;
 // "CC: somebodyelse@example.com";
+$body  = $body.' Phone : '.$phone;
 
 mail($to,$subject,$body,$headers); 
-header('location:index.html');
+header('location:contactus.html');
 
 ?>
